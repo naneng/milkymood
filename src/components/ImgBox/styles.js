0243b3ea09203b4389a773ecfc67ad987eb2769f
Katrina Ariola <kat3ariola@gmail.com>
2023-08-30 16:01:05 -0700

    added lodash pkg and updated image rendering issues

--- src/components/ImgBox/styles.js
+++ src/components/ImgBox/styles.js
@@ -7,12 +7,12 @@ export const Image = styled.div.attrs(({ $movedX, $movedY }) => ({
 }))`
   /* transform: ${({ $movedX, $movedY }) =>
     `translate(${$movedX}px, ${$movedY}px)`}; */
-  background-image: url("/image1.png");
+  /* background-image: url("/image1.jpeg"); */
+  background-image: url(${process.env.PUBLIC_URL}/image1.png);
   background-position: ${({ $initialX, $initialY }) =>
     `-${$initialX * 100}px -${$initialY * 100}px`};
   background-size: 400px 600px;
   height: 100px;
   width: 100px;
   position: relative;
-  
 `;
